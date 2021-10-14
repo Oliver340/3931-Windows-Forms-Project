e26980a9c994d2d74cd6ccc6a7fcc0cbe5cc678c
Oliver Harrison <oliverharrison255@gmail.com>
2021-10-14 11:27:13 -0700

    added the fourier and filtering

---
+++ 3931 Project windows forms/Filtering.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3931_Project_windows_forms
+{
+    class Filtering
+    {
+        public static double[] convolution(double[] fw, double[] s)
+        {
+            double sum;
+            int difference = s.Length % fw.Length;
+            int sizeOfSamplesWithZero = s.Length + difference + 1;
+            double[] newSamples = new double[sizeOfSamplesWithZero];
+            for (int i = 0; i < sizeOfSamplesWithZero; i++)
+            {
+                newSamples[i] = 0;
+            }
+            for (int i = 0; i < s.Length; i++)
+            {
+                newSamples[i] = s[i];
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                sum = 0;
+                for (int j = 0; j < fw.Length; j++)
+                {
+                    sum += newSamples[i + j] * fw[j];
+                }
+                s[i] = sum;
+            }
+            return s;
+        }
+    }
+}
---
+++ 3931 Project windows forms/Fourier.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3931_Project_windows_forms
+{
+    public struct complex
+    {
+        public double re;
+        public double im;
+    }
+
+    public static class Fourier
+    {
+
+        public static complex[] DFT(double[] s, int N)
+        {
+            complex[] A = new complex[N];
+
+            for (int f = 0; f < N; f++)
+            {
+                A[f] = new complex();
+                A[f].im = 0;
+                A[f].re = 0;
+                for (int t = 0; t < N; t++)
+                {
+                    A[f].re += s[t] * Math.Cos(2.0 * Math.PI * t * f / N);
+                    A[f].im -= s[t] * Math.Sin(2.0 * Math.PI * t * f / N);
+                }
+            }
+            return A;
+        }
+
+        public static double[] inverseDFT(complex[] A, int N)
+        {
+            double[] s = new double[N];
+
+            for (int t = 0; t < N; t++)
+            {
+                s[t] = 0;
+                for (int f = 0; f < N; f++)
+                {
+                    s[t] += A[f].re * Math.Cos(2 * Math.PI * t * f / N);
+                    s[t] -= A[f].im * Math.Sin(2 * Math.PI * t * f / N);
+                }
+            }
+            return s;
+        }
+        public static void divideByN(complex[] A, int N)
+        {
+            for (int i = 0; i < A.Length; ++i)
+            {
+                A[i].re /= N;
+                A[i].im /= N;
+            }
+        }
+
+    }
+}
